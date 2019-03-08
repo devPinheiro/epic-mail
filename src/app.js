d1b3f572226c2ef8e5135a0fd83b3c50371e3d31
Samuel <pinheirolaoluwa@gmail.com>
2019-03-08 01:05:11 +0100

    changed the deploy config

--- src/app.js
+++ src/app.js
@@ -1,37 +1,37 @@
-import express from "express";
-import logger from "morgan";
-import { apiRouter }  from "./api/V1";
+import express from 'express';
+import logger from 'morgan';
+import { apiRouter } from './api/V1';
 
 /**
- * 
+ *
  * instantiate express app
  *  */
 const app = express();
 
 
 /**
- * 
+ *
  * setup port
  *  */
 const PORT = process.env.PORT || 4000;
-const hostname = "localhost";
+const hostname = 'localhost';
 
 /**
  * set up middleware
  * @method express.json() parses incoming requests with JSON payloads based on body-parser
- * 
+ *
  * @method express.urlencoded() parses incoming requests with urlencode payloads based on body-parser
  *  */
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
 
 // api endpoints
 app.use('/api/v1', apiRouter);
 
 
-app.listen(PORT, hostname,()=>{
-  console.log(`server listening on https://${hostname}:${PORT}`)
+app.listen(PORT, hostname, () => {
+  console.log(`server listening on https://${hostname}:${PORT}`);
 });
 
 export default app;
