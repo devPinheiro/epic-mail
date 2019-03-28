3fc7f18fcceaa4ea8bf03766f6364d5c2c4eb3f3
devPinheiro <pinheirolaoluwa@gmail.com>
2019-03-28 08:58:02 +0100

    ft(signup): user can signup on the app [Finishes #164952376]

--- UI/js/app.js
+++ UI/js/app.js
@@ -7,20 +7,43 @@ let sidebar = $('.sidebar'),
     openSidebar = $('.open-sidebar'),
     closeSidebar = $('.close-sidebar');
 
-// Menu Navigation
-openSidebar.addEventListener('click', () => {
-    // open
-    openSidebar.style.display = 'none';
-    closeSidebar.style.display = "block";
-    sidebar.style.width = "200px";
-    main.style.marginLeft = "200px";
+// // Menu Navigation
+// openSidebar.onClick('click', () => {
+//     // open
+//     openSidebar.style.display = 'none';
+//     closeSidebar.style.display = "block";
+//     sidebar.style.width = "200px";
+//     main.style.marginLeft = "200px";
 
-});
+// });
 
+//     // close
+// closeSidebar.addEventListener('click', () => {
+//     sidebar.style.width = "0px";
+//     main.style.marginLeft = "0px";
+//     openSidebar.style.display = 'block';
+//     closeSidebar.style.display = "none";
+// });
 
-closeSidebar.addEventListener('click', () => {
-    sidebar.style.width = "0px";
-    main.style.marginLeft = "0px";
-    openSidebar.style.display = 'block';
-    closeSidebar.style.display = "none";
-});
+// API library
+class Samios {
+    // static method for post requests
+    static async signup(payload) {
+        const signupR = await fetch('https://epic-mail-devp.herokuapp.com/api/v1/auth/signup',  {
+                method: "post",
+                mode: "cors",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(payload)
+            });
+
+        const signupResult = await signupR.json()
+
+        return {
+            signupResult
+        }
+    }
+
+}
--- UI/js/index_2.js
+++ UI/js/index_2.js
@@ -1,37 +1,103 @@
 
-// let use something different
-let $ = selector => document.querySelector(selector);
-
 // select element needed
 let container = $("#login-form"),
     form = $('.form'),
     container_signup = $("#signup_cont"),
     form_signup = $('#signup-form');
+
+
 // Listen for submit event -- Sign Up
-$('#signup-form').addEventListener('submit', (e) => {
+$('#signup_btn').onclick = (e) => {
+    
     e.preventDefault();
-
     // get user's input
-    const fname = $("#fname").value,
-        lname = $('#lname').value,
-        email = $("#s_email").value,
-        password = $("#s_password").value;
+    const payload = {
+        firstName: $("#fname").value,
+        lastName: $('#lname').value,
+        email: $("#s_email").value,
+        password: $("#s_password").value
+    }
 
-    // let's do a little validation
-    if (email !== '') {
+    // first validate user input
+     // let's do a little validation
+     let regMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+     let reg = /[A-Za-z]/;
+     let checkEmail = regMail.test(payload.email); // returns a boolean 
+     let checkFirstname = reg.test(payload.firstName); // returns a boolean 
+     let checkLastname = reg.test(payload.lastName); // returns a boolean 
+     let checkPassword = reg.test(payload.password); // returns a boolean 
 
-        showAlertSignup("success", `Successfully Registered`);
-        // Timeout after 5s
-        setTimeout(function () {
-            window.location = 'inbox.html';
-        }, 3000);
+      $('#s_email').style.border = '';
+      $('#s_password').style.border = '';
+      $('#fname').style.border = '';
+      $('#lname').style.border = '';
+     if (!checkEmail) {
+         $('#s_email').style.border = "1px solid #ff1e1e";
+         showAlertSignup('error', 'provide correct email credential')
+     }
 
+    if (!checkFirstname) {
+         $('#fname').style.border = "1px solid #ff1e1e";
+         showAlertSignup('error', 'firstname is incorrect')
+     }
+
+     if (!checkLastname) {
+         $('#lname').style.border = "1px solid #ff1e1e";
+         showAlertSignup('error', 'lastname is incorrect')
+     }
+
+     if (!checkPassword) {
+         $('#s_password').style.border = "1px solid #ff1e1e";
+         showAlertSignup('error', 'password requires min 6 characters')
+     }
+
+    // let's sign user up
+    if(checkEmail && checkFirstname && checkLastname && checkPassword){
+      signUp(payload);
+    }
+    
+};
+
+// sign user up
+const signUp = async (body) => {
+
+    if (body) {
+
+        // disable button
+        $('#signup_btn').disabled = true;
+        $('#signup_btn').style.backgroundColor = '#c0c0c0';
+        // make network request
+        const { signupResult } = await Samios.signup(body);
+
+        if(signupResult.error){
+            let errMsg;
+            if(signupResult.status === 409){
+                errMsg = signupResult.error;
+            }
+            if(signupResult.status === 400){
+                    errMsg = Object.values(signupResult.error).join(' \n \n')
+            }
+           showAlertSignup('error', errMsg);
+           // enable button
+           $('#signup_btn').disabled = false;
+           $('#signup_btn').style.backgroundColor = '#e68016';
+        } else{
+            console.log(signupResult.data.token);
+            // store token into localstorage
+            localStorage.setItem('token', signupResult.data.token);
+            showAlertSignup("success", `Successfully Registered`);
+            // enable button
+            $('#signup_btn').disabled = false;
+            $('#signup_btn').style.backgroundColor = '#e68016';
+            // Timeout after 5s
+            setTimeout(function () {
+                window.location = 'inbox.html';
+            }, 3000);
+        }
     } else {
         showAlertSignup('error', 'Something is wrong with your credentials')
     }
-
-
-});
+}
 
 // create an alert element
 let showAlertSignup = (classN, message) => {
@@ -49,5 +115,5 @@ let showAlertSignup = (classN, message) => {
     // Timeout after 5s
     setTimeout(function () {
         $('.alert').remove();
-    }, 5000);
+    }, 10000);
 }
